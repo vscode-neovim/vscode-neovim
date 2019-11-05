2851e81c9ee20f726c671ca78a76b6626b59afe2
Alexey Svetlyakov <devpieron@gmail.com>
2019-11-05 17:28:54 +0100

    Added scroll reveal test

--- src/test/suite/vscode-integartion-specific.test.ts
+++ src/test/suite/vscode-integartion-specific.test.ts
@@ -1,6 +1,9 @@
+import path from "path";
+import { strict as assert } from "assert";
+
 import vscode from "vscode";
 
-import { attachTestNvimClient, assertContent, wait, setCursor } from "../utils";
+import { attachTestNvimClient, assertContent, wait, setCursor, closeActiveEditor, sendVSCodeKeys } from "../utils";
 
 describe("VSCode integration specific stuff", () => {
     vscode.window.showInformationMessage("Yank & paste test");
@@ -25,6 +28,7 @@ describe("VSCode integration specific stuff", () => {
             },
             client,
         );
+        await closeActiveEditor(client);
     });
 
     it("Moves on cursor on go definition", async () => {
@@ -45,5 +49,23 @@ describe("VSCode integration specific stuff", () => {
             },
             client,
         );
+        await closeActiveEditor(client);
+    });
+
+    it("Editor cursor revealing", async () => {
+        await wait();
+        const doc = await vscode.workspace.openTextDocument(path.join(__dirname, "../../../test_data/scrolltest.txt"));
+        await vscode.window.showTextDocument(doc, vscode.ViewColumn.One);
+        await wait();
+
+        await sendVSCodeKeys("130j");
+        await assertContent({ cursor: [130, 0] }, client);
+
+        let range = vscode.window.activeTextEditor!.visibleRanges[0];
+        assert.ok(range.start.line <= 129);
+
+        await sendVSCodeKeys("40k");
+        range = vscode.window.activeTextEditor!.visibleRanges[0];
+        assert.ok(range.start.line <= 89);
     });
 });
