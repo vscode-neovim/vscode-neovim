2fe8dbfb2abf5bab14f3abd5efcb78fcfdc6849c
Alexey Svetlyakov <devpieron@gmail.com>
2019-11-12 04:57:58 +0100

    Add test for external changes

--- src/test/suite/external-changes.test.ts
+++ src/test/suite/external-changes.test.ts
@@ -1,3 +1,74 @@
+import os from "os";
+import path from "path";
+import fs from "fs";
+
+import vscode from "vscode";
+import { NeovimClient } from "neovim";
+
+import { attachTestNvimClient, closeNvimClient, closeAllActiveEditors, assertContent, wait } from "../utils";
+
 describe("External changes on file", () => {
-    it("Stub", () => {});
+    let client: NeovimClient;
+    before(async () => {
+        client = await attachTestNvimClient();
+    });
+    after(async () => {
+        await closeNvimClient(client);
+    });
+
+    afterEach(async () => {
+        await closeAllActiveEditors();
+    });
+
+    it("Multiple replacing/deleting/inserting", async () => {
+        const filePath = path.join(os.tmpdir(), Math.random().toString());
+        fs.writeFileSync(filePath, ["line 1", "line 2", "line 3", "line 4", "line 5", "line 6", "line 7"].join("\n"), {
+            encoding: "utf8",
+        });
+
+        const doc = await vscode.workspace.openTextDocument(filePath);
+        await vscode.window.showTextDocument(doc);
+        await wait(1000);
+
+        await assertContent(
+            {
+                content: ["line 1", "line 2", "line 3", "line 4", "line 5", "line 6", "line 7"],
+            },
+            client,
+        );
+
+        fs.writeFileSync(
+            filePath,
+            [
+                "line1 changed - line 2 deleted",
+                "line 3",
+                "line 3.1",
+                "line 3.2",
+                "line 4",
+                "line6",
+                "line 6.1",
+                "line 7",
+                "line 8",
+            ].join("\n"),
+        );
+        await wait(2000);
+
+        await assertContent(
+            {
+                content: [
+                    "line1 changed - line 2 deleted",
+                    "line 3",
+                    "line 3.1",
+                    "line 3.2",
+                    "line 4",
+                    "line6",
+                    "line 6.1",
+                    "line 7",
+                    "line 8",
+                ],
+            },
+            client,
+        );
+        fs.unlinkSync(filePath);
+    });
 });
