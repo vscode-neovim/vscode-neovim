8627667d534aa41cd8c2ed3212096f565aeec09e
Jack Garner <jacksonhgarner@outlook.com>
2020-08-04 17:46:47 -0600

    Fix a merge conflict and remove extra files

--- src/highlight_provider.ts
+++ src/highlight_provider.ts
@@ -412,11 +412,7 @@ export class HighlightProvider {
                 continue;
             }
             const decoratorRanges = ranges.map(
-<<<<<<< HEAD
-                r => new Range(topLine + r.lineS, r.colS, topLine + r.lineE, r.colE + 1)
-=======
                 (r) => new Range(topLine + r.lineS, r.colS, topLine + r.lineE, r.colE + 1),
->>>>>>> 54e46eac62a9c537bad55073293763ff434982f1
             );
             result.push([decorator, decoratorRanges]);
         }
