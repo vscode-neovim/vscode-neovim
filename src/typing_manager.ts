7b1a6af2cbe739d0d532cc647c63a3c35342fef0
Shatur95 <genaloner@gmail.com>
2020-09-22 01:30:12 +0300

    Move pendingKeysAfterEnter outside of condition

--- src/typing_manager.ts
+++ src/typing_manager.ts
@@ -76,10 +76,10 @@ export class TypingManager implements Disposable {
                         this.logger.debug(`${LOG_PREFIX}: Waiting done, disposing type handler`);
                         this.typeHandlerDisposable.dispose();
                         this.typeHandlerDisposable = undefined;
-                        if (this.pendingKeysAfterEnter) {
-                            commands.executeCommand("default:type", { text: this.pendingKeysAfterEnter });
-                            this.pendingKeysAfterEnter = "";
-                        }
+                    }
+                    if (this.pendingKeysAfterEnter) {
+                        commands.executeCommand("default:type", { text: this.pendingKeysAfterEnter });
+                        this.pendingKeysAfterEnter = "";
                     }
                 });
             } else {
