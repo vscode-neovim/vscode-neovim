22288abea466d5e20fde5520e87d616426123ccb
joshuabrink1 <joshuabrink1@gmail.com>
2021-08-16 22:42:43 +0200

    Retain initial command input

--- src/command_line.ts
+++ src/command_line.ts
@@ -20,6 +20,10 @@ export class CommandLineController implements Disposable {
 
     private completionTimer?: NodeJS.Timeout;
 
+    private currentCommand: string = "";
+
+    private historyLocation: number = 0;
+
     private completionItems: QuickPickItem[] = [];
 
     private mode = "";
@@ -109,6 +113,9 @@ export class CommandLineController implements Disposable {
     private onChange = (e: string): void => {
         if (!this.isDisplayed) {
             return;
+        }
+        if(this.historyLocation == 0) {
+            this.currentCommand = e;
         }
         const mode = this.mode;
         if (mode === ":" && (e.charAt(0) === "?" || e.charAt(0) === "/") && this.input.items.length) {
@@ -226,6 +233,7 @@ export class CommandLineController implements Disposable {
         await this.neovimClient.input("<S-Up>");
         const res = await this.neovimClient.callFunction("getcmdline", []);
         if (res) {
+            this.historyLocation++;
             this.input.value = res;
             this.input.show();
         }
@@ -235,7 +243,13 @@ export class CommandLineController implements Disposable {
         await this.neovimClient.input("<S-Down>");
         const res = await this.neovimClient.callFunction("getcmdline", []);
         if (res) {
-            this.input.value = res;
+            if (this.historyLocation > 0) {
+                this.historyLocation--;
+                this.input.value = res;
+            }
+            if(this.historyLocation == 0) {
+               this.input.value = this.currentCommand; 
+            }
             this.input.show();
         }
     };
