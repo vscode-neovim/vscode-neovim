3c3be8b0ee99e9cae457aaf5bc6bde1231936bc4
Theo Lemay <theol0403@gmail.com>
2022-06-30 00:14:56 -0700

    experimental: listen to typing in insert mode

--- src/document_change_manager.ts
+++ src/document_change_manager.ts
@@ -119,17 +119,7 @@ export class DocumentChangeManager implements Disposable, NeovimExtensionRequest
         return Promise.all(promises.map((p) => p.promise).filter(Boolean));
     }
 
-    public hasDocumentChangeCompletionLock(doc: TextDocument): boolean {
-        return (this.textDocumentChangePromise.get(doc)?.length || 0) > 0;
-    }
-
-    public async handleExtensionRequest(name: string, args: unknown[]): Promise<void> {
-        if (name === "insert-line") {
-            const [type] = args as ["before" | "after"];
-            this.dotRepeatStartModeInsertHint = type === "before" ? "O" : "o";
-            this.logger.debug(`${LOG_PREFIX}: Setting start insert mode hint - ${this.dotRepeatStartModeInsertHint}`);
-        }
-    }
+    public async handleExtensionRequest(name: string, args: unknown[]): Promise<void> {}
 
     public async syncDocumentsWithNeovim(): Promise<void> {
         this.logger.debug(`${LOG_PREFIX}: Syncing document changes with neovim`);
@@ -230,68 +220,6 @@ export class DocumentChangeManager implements Disposable, NeovimExtensionRequest
             return;
         }
         await callAtomic(this.client, requests, this.logger, LOG_PREFIX);
-    }
-
-    public async syncDotRepatWithNeovim(): Promise<void> {
-        // dot-repeat executes last change across all buffers. So we'll create a temporary buffer & window,
-        // replay last changes here to trick neovim and destroy it after
-        if (!this.dotRepeatChange) {
-            return;
-        }
-        this.logger.debug(`${LOG_PREFIX}: Syncing dot repeat`);
-        const dotRepeatChange = { ...this.dotRepeatChange };
-        this.dotRepeatChange = undefined;
-
-        const currWin = await this.client.window;
-
-        // temporary buffer to replay the changes
-        const buf = await this.client.createBuffer(false, true);
-        if (typeof buf === "number") {
-            return;
-        }
-        // create temporary win
-        await this.client.setOption("eventignore", "BufWinEnter,BufEnter,BufLeave");
-        const win = await this.client.openWindow(buf, true, {
-            external: true,
-            width: 100,
-            height: 100,
-        });
-        await this.client.setOption("eventignore", "");
-        if (typeof win === "number") {
-            return;
-        }
-        const edits: [string, unknown[]][] = [];
-
-        // for delete changes we need an actual text, so let's prefill with something
-        // accumulate all possible lengths of deleted text to be safe
-        const delRangeLength = dotRepeatChange.rangeLength;
-        if (delRangeLength) {
-            const stub = new Array(delRangeLength).fill("x").join("");
-            edits.push(
-                ["nvim_buf_set_lines", [buf.id, 0, 0, false, [stub]]],
-                ["nvim_win_set_cursor", [win.id, [1, delRangeLength]]],
-            );
-        }
-        let editStr = "";
-        if (dotRepeatChange.startMode) {
-            editStr += `<Esc>${dotRepeatChange.startMode}`;
-            // remove EOL from first change
-            if (dotRepeatChange.text.startsWith(dotRepeatChange.eol)) {
-                dotRepeatChange.text = dotRepeatChange.text.slice(dotRepeatChange.eol.length);
-            }
-        }
-        if (dotRepeatChange.rangeLength) {
-            editStr += [...new Array(dotRepeatChange.rangeLength).keys()].map(() => "<BS>").join("");
-        }
-        editStr += dotRepeatChange.text.split(dotRepeatChange.eol).join("\n").replace("<", "<LT>");
-        edits.push(["nvim_input", [editStr]]);
-        // since nvim_input is not blocking we need replay edits first, then clean up things in subsequent request
-        await callAtomic(this.client, edits, this.logger, LOG_PREFIX);
-
-        const cleanEdits: [string, unknown[]][] = [];
-        cleanEdits.push(["nvim_set_current_win", [currWin.id]]);
-        cleanEdits.push(["nvim_win_close", [win.id, true]]);
-        await callAtomic(this.client, cleanEdits, this.logger, LOG_PREFIX);
     }
 
     private onBufferInit: BufferManager["onBufferInit"] = (id, doc) => {
@@ -589,8 +517,6 @@ export class DocumentChangeManager implements Disposable, NeovimExtensionRequest
             }
         }
         this.changedDocuments.add(document);
-        if (!this.modeManager.isInsertMode) {
-            this.syncDocumentsWithNeovim();
-        }
+        this.syncDocumentsWithNeovim();
     };
 }
--- src/typing_manager.ts
+++ src/typing_manager.ts
@@ -14,26 +14,6 @@ export class TypingManager implements Disposable {
      * Separate "type" command disposable since we init/dispose it often
      */
     private typeHandlerDisposable?: Disposable;
-    /**
-     * Flag indicating that we're going to exit insert mode and sync buffers into neovim
-     */
-    private isExitingInsertMode = false;
-    /**
-     * Flag indicating that we're going to enter insert mode and there are pending document changes
-     */
-    private isEnteringInsertMode = false;
-    /**
-     * Additional keys which were pressed after exiting insert mode. We'll replay them after buffer sync
-     */
-    private pendingKeysAfterExit = "";
-    /**
-     * Additional keys which were pressed after entering the insert mode
-     */
-    private pendingKeysAfterEnter = "";
-    /**
-     * Timestamp when the first composite escape key was pressed. Using timestamp because timer may be delayed if the extension host is busy
-     */
-    private compositeEscapeFirstPressTimestamp?: number;
 
     public constructor(
         private logger: Logger,
@@ -42,20 +22,7 @@ export class TypingManager implements Disposable {
         private changeManager: DocumentChangeManager,
     ) {
         this.typeHandlerDisposable = commands.registerTextEditorCommand("type", this.onVSCodeType);
-        this.disposables.push(
-            commands.registerCommand("vscode-neovim.sync-send", (key) => this.onSyncSendCommand(key)),
-        );
         this.disposables.push(commands.registerCommand("vscode-neovim.escape", this.onEscapeKeyCommand));
-        this.disposables.push(
-            commands.registerCommand("vscode-neovim.compositeEscape1", (key: string) =>
-                this.handleCompositeEscapeFirstKey(key),
-            ),
-        );
-        this.disposables.push(
-            commands.registerCommand("vscode-neovim.compositeEscape2", (key: string) =>
-                this.handleCompositeEscapeSecondKey(key),
-            ),
-        );
         this.modeManager.onModeChange(this.onModeChange);
     }
 
@@ -65,97 +32,17 @@ export class TypingManager implements Disposable {
     }
 
     private onModeChange = (): void => {
-        if (this.modeManager.isInsertMode && this.typeHandlerDisposable && !this.modeManager.isRecordingInInsertMode) {
-            this.pendingKeysAfterEnter = "";
-            const editor = window.activeTextEditor;
-            if (editor && this.changeManager.hasDocumentChangeCompletionLock(editor.document)) {
-                this.isEnteringInsertMode = true;
-                this.logger.debug(
-                    `${LOG_PREFIX}: Waiting for document completion operation before disposing type handler`,
-                );
-                this.changeManager.getDocumentChangeCompletionLock(editor.document)?.then(() => {
-                    this.isEnteringInsertMode = false;
-                    if (this.typeHandlerDisposable && this.modeManager.isInsertMode) {
-                        this.logger.debug(`${LOG_PREFIX}: Waiting done, disposing type handler`);
-                        this.typeHandlerDisposable.dispose();
-                        this.typeHandlerDisposable = undefined;
-                    }
-                    if (this.pendingKeysAfterEnter) {
-                        commands.executeCommand(this.modeManager.isInsertMode ? "default:type" : "type", {
-                            text: this.pendingKeysAfterEnter,
-                        });
-                        this.pendingKeysAfterEnter = "";
-                    }
-                });
-            } else {
-                this.logger.debug(`${LOG_PREFIX}: Disposing type handler`);
-                this.typeHandlerDisposable.dispose();
-                this.typeHandlerDisposable = undefined;
-            }
-        } else if (!this.modeManager.isInsertMode) {
-            this.isEnteringInsertMode = false;
-            this.isExitingInsertMode = false;
-            if (!this.typeHandlerDisposable) {
-                this.logger.debug(`${LOG_PREFIX}: Enabling type handler`);
-                this.typeHandlerDisposable = commands.registerTextEditorCommand("type", this.onVSCodeType);
-            }
+        if (!this.typeHandlerDisposable) {
+            this.logger.debug(`${LOG_PREFIX}: Enabling type handler`);
+            this.typeHandlerDisposable = commands.registerTextEditorCommand("type", this.onVSCodeType);
         }
     };
 
     private onVSCodeType = (_editor: TextEditor, edit: TextEditorEdit, type: { text: string }): void => {
-        if (this.isExitingInsertMode || this.isEnteringInsertMode) {
-            this.pendingKeysAfterExit += type.text;
-        } else {
-            this.client.input(normalizeInputString(type.text, !this.modeManager.isRecordingInInsertMode));
-        }
-    };
-
-    private onSyncSendCommand = async (key: string): Promise<void> => {
-        this.logger.debug(`${LOG_PREFIX}: Sync and send for: ${key}`);
-        if (this.modeManager.isInsertMode) {
-            this.logger.debug(`${LOG_PREFIX}: Syncing buffers with neovim (${key})`);
-            // rebind early to store fast pressed keys which may happen between sending changes to neovim and exiting insert mode
-            // see https://github.com/asvetliakov/vscode-neovim/issues/324
-            if (!this.typeHandlerDisposable) {
-                this.typeHandlerDisposable = commands.registerTextEditorCommand("type", this.onVSCodeType);
-            }
-            // this.leaveMultipleCursorsForVisualMode = false;
-            await this.changeManager.syncDocumentsWithNeovim();
-            await this.changeManager.syncDotRepatWithNeovim();
-        }
-        const keys = normalizeInputString(this.pendingKeysAfterExit);
-        this.logger.debug(`${LOG_PREFIX}: Pending keys sent with ${key}: ${keys}`);
-        this.pendingKeysAfterExit = "";
-        await this.client.input(`${key}${keys}`);
+        this.client.input(normalizeInputString(type.text, !this.modeManager.isRecordingInInsertMode));
     };
 
     private onEscapeKeyCommand = async (): Promise<void> => {
-        if (this.modeManager.isInsertMode) this.isExitingInsertMode = true;
-        await this.onSyncSendCommand("<Esc>");
-    };
-
-    private handleCompositeEscapeFirstKey = async (key: string): Promise<void> => {
-        const now = new Date().getTime();
-        if (this.compositeEscapeFirstPressTimestamp && now - this.compositeEscapeFirstPressTimestamp <= 200) {
-            // jj
-            this.compositeEscapeFirstPressTimestamp = undefined;
-            await commands.executeCommand("deleteLeft");
-            await this.onEscapeKeyCommand();
-        } else {
-            this.compositeEscapeFirstPressTimestamp = now;
-            // insert character
-            await commands.executeCommand("default:type", { text: key });
-        }
-    };
-
-    private handleCompositeEscapeSecondKey = async (key: string): Promise<void> => {
-        const now = new Date().getTime();
-        if (this.compositeEscapeFirstPressTimestamp && now - this.compositeEscapeFirstPressTimestamp <= 200) {
-            this.compositeEscapeFirstPressTimestamp = undefined;
-            await commands.executeCommand("deleteLeft");
-            await this.onEscapeKeyCommand();
-        } else {
-            await commands.executeCommand("default:type", { text: key });
-        }
+        await this.client.input("<Esc>");
     };
 }
