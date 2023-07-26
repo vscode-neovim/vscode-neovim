89c039865e70b029535a0d53c9ef9ffdb1a65a56
Theo Lemay <theol0403@gmail.com>
2023-07-26 09:43:02 -0700

    fix: don't expose internal api

--- runtime/lua/vscode.lua
+++ runtime/lua/vscode.lua
@@ -6,8 +6,6 @@ local M = {}
 
 M.notify = api.notify
 M.call = api.call
-M.notify_extension = api.notify_extension
-M.call_extension = api.call_extension
 
 M.setup = function()
     defaults.setup()
