056bb0a91e292cbd4d2a9c994ec3db114dc53a58
Theo Lemay <theol0403@gmail.com>
2023-07-29 17:30:39 -0700

    feat: add lua api

--- runtime/lua/vscode-neovim.lua
+++ runtime/lua/vscode-neovim.lua
@@ -6,6 +6,10 @@ local M = {}
 
 M.notify = api.notify
 M.call = api.call
+M.call_range = api.call_range
+M.notify_range = api.notify_range
+M.call_range_pos = api.call_range_pos
+M.notify_range_pos = api.notify_range_pos
 
 M.setup = function()
     defaults.setup()
