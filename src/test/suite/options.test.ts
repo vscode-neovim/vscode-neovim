f3bc0871309c5c3c7377acf4e90268e70741ee04
wongxy <xiyao.wong@foxmail.com>
2023-09-05 20:13:45 +0800

    ci: add test

---
+++ src/test/suite/options.test.ts
@@ -0,0 +1,49 @@
+import { strict as assert } from "assert";
+
+import { NeovimClient } from "neovim";
+import vscode from "vscode";
+
+import {
+    attachTestNvimClient,
+    closeAllActiveEditors,
+    closeNvimClient,
+    openTextDocument,
+    sendEscapeKey,
+    sendVSCodeKeys,
+    setCursor,
+    wait,
+} from "../utils";
+
+describe("Tab options test", () => {
+    let client: NeovimClient;
+    before(async () => {
+        client = await attachTestNvimClient();
+        await client.command("augroup TestOptions");
+        await client.command("autocmd InsertEnter * set nornu");
+        await client.command("autocmd InsertLeave * set rnu");
+        await client.command("augroup END");
+    });
+    after(async () => {
+        await client.command("augroup TestOptions");
+        await client.command("autocmd!");
+        await client.command("augroup END");
+        await closeNvimClient(client);
+        await closeAllActiveEditors();
+    });
+
+    it("number & relativenumber", async () => {
+        const editor = await openTextDocument({ content: "testing...\n".repeat(10) });
+        await wait();
+
+        await setCursor(3, 0);
+        await wait();
+
+        await sendVSCodeKeys("i");
+        await wait();
+        assert.equal(editor.options.lineNumbers, vscode.TextEditorLineNumbersStyle.On);
+
+        await sendEscapeKey();
+        await wait();
+        assert.equal(editor.options.lineNumbers, vscode.TextEditorLineNumbersStyle.Relative);
+    });
+});
