7703332dcebb48a018ab77d526150def67a6fa82
wongxy <xiyao.wong@foxmail.com>
2023-10-08 08:30:15 +0800

    chore: add config to the context subscriptions

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,10 @@
 import * as vscode from "vscode";
 
+import { config } from "./config";
 import { MainController } from "./main_controller";
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
+    context.subscriptions.push(config);
     try {
         const plugin = new MainController(context.extensionPath.replace(/\\/g, "\\\\"));
         context.subscriptions.push(plugin);
