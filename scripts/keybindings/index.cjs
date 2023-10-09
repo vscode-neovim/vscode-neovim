2ea92724c8b2adae2f811ddc2f0b967c721fbb6c
wongxy <xiyao.wong@foxmail.com>
2023-10-09 11:55:45 +0800

    refactor: use scripts to maintain keybindings (#1508)

---
+++ scripts/keybindings/index.cjs
@@ -0,0 +1,17 @@
+async function main() {
+    const path = await import("path");
+    const fs = await import("fs");
+    const packageJsonPath = path.join(__filename, "../../../package.json");
+    const packageJson = require(packageJsonPath);
+
+    const keybindings = [];
+    fs.readdirSync(__dirname)
+        .filter((f) => /^\d+.*?\.cjs$/.test(f))
+        .sort((a, b) => +a.match(/\d+/)[0] - +b.match(/\d+/)[0])
+        .map((m) => keybindings.push(...require(path.join(__dirname, m))));
+    packageJson.contributes.keybindings = keybindings;
+
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 4) + "\n");
+}
+
+main();
