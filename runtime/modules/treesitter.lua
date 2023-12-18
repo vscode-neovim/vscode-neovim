c2ca1be787c9114bdb7dcc57e47d1932cd9fb878
wongxy <xiyao.wong@foxmail.com>
2023-12-18 23:27:03 +0800

    fix(treesitter): manually trigger parsing (#1713)

---
+++ runtime/modules/treesitter.lua
@@ -0,0 +1,20 @@
+-- TODO: Figure out why Treesitter doesn't parse.
+vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, {
+  group = vim.api.nvim_create_augroup("VSCodeAutoTresitterParse", {}),
+  callback = (function()
+    local timer
+    return function()
+      if timer and timer:is_active() then
+        timer:close()
+      end
+      timer = vim.defer_fn(function()
+        if vim.b._vscode_last_parse_changedtick ~= vim.b.changedtick then
+          vim.b._vscode_last_parse_changedtick = vim.b.changedtick
+          pcall(function()
+            vim.treesitter.get_parser():parse()
+          end)
+        end
+      end, 500)
+    end
+  end)(),
+})
