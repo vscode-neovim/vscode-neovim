2186f1749c708e48574eb7245b1de3485cf34317
Matthew Broadway <mattdbway@gmail.com>
2024-03-17 13:26:53 +0000

    feat: allow running single tests using an environment variable

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -5,6 +5,13 @@ import Mocha from "mocha";
 import "source-map-support/register";
 
 export async function run(): Promise<void> {
+    let test_regex = process.env.VSCODE_NEOVIM_TEST_REGEX;
+    if (test_regex !== undefined) {
+        console.log(`running tests by regex: ${test_regex}`);
+    } else {
+        test_regex = ".*";
+    }
+
     // Create the mocha test
     const mocha = new Mocha({
         ui: "bdd",
@@ -12,7 +19,7 @@ export async function run(): Promise<void> {
         bail: false,
         slow: 20000,
         fullTrace: true,
-        grep: ".*",
+        grep: test_regex,
         retries: 2,
     });
     const testsRoot = path.resolve(__dirname, "..");
