824f064970120bed68d2478d2145f06e4a5ce961
wongxy <xiyao.wong@foxmail.com>
2024-05-18 12:05:54 +0800

    test: add test

--- src/test/integ/highlights.test.ts
+++ src/test/integ/highlights.test.ts
@@ -1,11 +1,18 @@
 import assert from "assert";
 
 import { NeovimClient } from "neovim";
-import vscode, { DecorationOptions, Position, window } from "vscode";
+import vscode, { DecorationOptions, Position, TextEditor, window } from "vscode";
 
-import { attachTestNvimClient, closeAllActiveEditors, closeNvimClient, wait } from "./integrationUtils";
+import {
+    attachTestNvimClient,
+    closeAllActiveEditors,
+    closeNvimClient,
+    sendEscapeKey,
+    sendNeovimKeys,
+    wait,
+} from "./integrationUtils";
 
-describe("Test ext mark", () => {
+describe("Test highlights", () => {
     let client: NeovimClient;
     let realWindow: typeof vscode.window | undefined;
 
@@ -21,9 +28,14 @@ describe("Test ext mark", () => {
         client = await attachTestNvimClient();
     });
 
+    beforeEach(async () => {
+        await closeAllActiveEditors();
+    });
+
     after(async () => {
         restoreWindow();
         await closeNvimClient(client);
+        await closeAllActiveEditors();
     });
 
     afterEach(() => {
@@ -37,12 +49,9 @@ describe("Test ext mark", () => {
         await wait(500);
         await vscode.window.showTextDocument(doc);
 
-        const stubTextEditor = new TextEditorStub();
         const curEditor = vscode.window.activeTextEditor;
         assert.ok(curEditor != null);
-        stubTextEditor.document = curEditor.document;
-        stubTextEditor.options = curEditor.options;
-        stubTextEditor.viewColumn = curEditor.viewColumn!;
+        const stubTextEditor = new TextEditorStub(curEditor);
         realWindow = vscode.window;
         vscode.window = {
             activeTextEditor: stubTextEditor,
@@ -74,25 +83,83 @@ describe("Test ext mark", () => {
         assert.ok(decoration.renderOptions?.before?.contentText == "j");
         assert.ok(decoration.renderOptions?.before?.color == "#ff0000");
     });
+
+    it("forward search / for long line", async () => {
+        const doc = await vscode.workspace.openTextDocument({
+            content: ["hello", " ".repeat(3000), "world", " ".repeat(1000)].join(""),
+        });
+        await vscode.window.showTextDocument(doc);
+
+        const curEditor = vscode.window.activeTextEditor;
+        assert.ok(curEditor != null);
+        const stubTextEditor = new TextEditorStub(curEditor);
+        realWindow = vscode.window;
+        vscode.window = {
+            ...realWindow,
+            activeTextEditor: stubTextEditor,
+            visibleTextEditors: [stubTextEditor],
+            createTextEditorDecorationType: window.createTextEditorDecorationType,
+        } as any;
+
+        {
+            await sendEscapeKey();
+            await sendNeovimKeys(client, "/orl");
+            await wait(500);
+            assert(stubTextEditor.decorationOptionsList.length > 0);
+            const decoration = stubTextEditor.decorationOptionsList[0][0] as DecorationOptions;
+            assert.ok(decoration.range.isEqual(new vscode.Range(0, 3006, 0, 3009)));
+        }
+    });
 });
 
 // https://github.com/VSCodeVim/Vim/blob/master/test/historyTracker.test.ts#L181
 // Fake class for testing
 /* eslint-disable */
 class TextEditorStub implements vscode.TextEditor {
-    document!: vscode.TextDocument;
-    selection!: vscode.Selection;
-    selections!: vscode.Selection[];
-    visibleRanges!: vscode.Range[];
-    options!: vscode.TextEditorOptions;
-    viewColumn!: vscode.ViewColumn;
-
     decorationOptionsList: Array<vscode.Range[] | vscode.DecorationOptions[]> = [];
 
-    constructor() {
-        this.selection = new vscode.Selection(0, 0, 0, 0);
-        this.selections = [this.selection];
+    get visibleRanges() {
+        return this.editor.visibleRanges;
     }
+
+    get selection() {
+        return this.editor.selection;
+    }
+
+    set selection(v) {
+        this.editor.selection = v;
+    }
+
+    get selections() {
+        return this.editor.selections;
+    }
+
+    set selections(v) {
+        this.editor.selections = v;
+    }
+
+    get document() {
+        return this.editor.document;
+    }
+
+    get options() {
+        return this.editor.options;
+    }
+
+    set options(v) {
+        this.editor.options = v;
+    }
+
+    get viewColumn() {
+        return this.editor.viewColumn;
+    }
+
+    get revealRange() {
+        return this.editor.revealRange;
+    }
+
+    constructor(private editor: TextEditor) {}
+
     async edit(
         callback: (editBuilder: vscode.TextEditorEdit) => void,
         options?: { undoStopBefore: boolean; undoStopAfter: boolean },
@@ -113,7 +180,6 @@ class TextEditorStub implements vscode.TextEditor {
     ) {
         this.decorationOptionsList.push(rangesOrOptions);
     }
-    revealRange(range: vscode.Range, revealType?: vscode.TextEditorRevealType) {}
     show(column?: vscode.ViewColumn) {}
     hide() {}
 }
