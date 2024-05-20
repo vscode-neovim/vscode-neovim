5b0eb4d98f9e7322f9b3c6377790163d5a6d0f45
wongxy <xiyao.wong@foxmail.com>
2024-05-20 11:30:40 +0800

    refactor(lua): move autocmds,filetype to runtime/modules

--- runtime/lua/vscode-neovim.lua
+++ runtime/lua/vscode-neovim.lua
@@ -3,15 +3,11 @@ local api = require("vscode-neovim.api")
 local default_optons = require("vscode-neovim.default-options")
 local cursor = require("vscode-neovim.cursor")
 local highlight = require("vscode-neovim.highlight")
-local filetype = require("vscode-neovim.filetype")
-local autocmds = require("vscode-neovim.autocmds")
 local sync_options = require("vscode-neovim.sync-options")
 
 default_optons.setup()
 cursor.setup()
 highlight.setup()
-filetype.setup()
-autocmds.setup()
 sync_options.setup()
 
 local vscode = {
