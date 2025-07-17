e906efc25a7318864b6eb28a8bca4a125d6fffbf
xiyaowong <xiyao.wong@foxmail.com>
2025-07-17 11:08:43 +0800

    fix: change the default statusline to an empty string

--- runtime/vscode/statusline.lua
+++ runtime/vscode/statusline.lua
@@ -1,26 +1,17 @@
 local api = vim.api
 
 local ns = api.nvim_create_namespace("vscode.statusline")
-
-local function get_status()
-  if vim.o.laststatus == 0 then
-    return ""
-  end
-
-  local info = api.nvim_get_option_info2("statusline", {})
-
-  if not info.was_set then
-    return ""
-  end
-
-  local str = api.nvim_eval_statusline(vim.o.statusline, {}).str
-  return str:gsub("\n", " "):gsub("%s+", " ")
-end
-
 local curr_status = ""
 
 local function refresh()
-  local status = get_status()
+  local status = ""
+
+  if vim.o.laststatus == 0 or vim.o.statusline == "" then
+    status = ""
+  else
+    local str = api.nvim_eval_statusline(vim.o.statusline, {}).str
+    status = str:gsub("\n", " "):gsub("%s+", " ")
+  end
 
   if curr_status ~= status then
     curr_status = status
