1a0b063dc8b41febb4be9df266d61533858f6654
wongxy <xiyao.wong@foxmail.com>
2026-09-11 20:03:15 +0800

    1

--- src/test/integ/highlights.test.ts
+++ src/test/integ/highlights.test.ts
@@ -110,6 +110,10 @@ describe("Test highlights", () => {
             await wait(500);
             assert(stubTextEditor.decorationOptionsList.length > 0);
             const decoration = stubTextEditor.decorationOptionsList[0][0] as DecorationOptions;
+            assert.equal(decoration.range.start.line, 0);
+            assert.equal(decoration.range.start.character, 3006);
+            assert.equal(decoration.range.end.line, 0);
+            assert.equal(decoration.range.end.character, 3009);
             assert.ok(decoration.range.isEqual(new vscode.Range(0, 3006, 0, 3009)));
         }
     });
